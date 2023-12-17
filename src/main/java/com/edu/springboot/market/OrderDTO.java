eb007217996e0339e9c2a17582db3cc77806f094
Yeseul679029 <syk679029@gmail.com>
2023-12-17 17:29:27 +0900

    관리자 주문내역 모달창 완료 커밋

--- src/main/java/com/edu/springboot/market/OrderDTO.java
+++ src/main/java/com/edu/springboot/market/OrderDTO.java
@@ -19,5 +19,11 @@ public class OrderDTO {
 	private String zipcode;
 	private String nickname;
 	private String state;
+	//모달창 join변수등록
+	private String cart_idx;
+	private String prod_idx;
+	private String prod_count;
+	private String prod_totprice;
+	private String prod_name;
 	
 }
