11ddf26f634b673a2eb272f10c6bc6ea3f848577
minseokminseok <kmt4208@gmail.com>
2023-12-17 15:56:46 +0900

    식당 커밋

--- src/main/java/com/edu/springboot/restaurant/RestaurantController.java
+++ src/main/java/com/edu/springboot/restaurant/RestaurantController.java
@@ -1,10 +1,13 @@
 package com.edu.springboot.restaurant;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,7 +28,32 @@ public class RestaurantController {
         model.addAttribute("restaurantList", restaurantList);
 
         System.out.println("성공?");
+        // 랜덤 숫자 생성
+        // 랜덤 숫자 생성
+        List<Integer> randomNumbers = generateRandomNumbers();
+        // 리스트의 첫 번째 값 가져오기 (랜덤한 숫자)
+        int randomNum = randomNumbers.get(0);
+        model.addAttribute("randomNum", randomNum);
+
+        System.out.println("성공?");
+        System.out.println("랜덤넘버");
+        System.out.println(randomNum);
 
         return "restaurant/restaurant_list";
     }
+    
+    @GetMapping("/generate")
+    public List<Integer> generateRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 7; i++) {
+            numbers.add(i);
+        }
+
+        Collections.shuffle(numbers);
+
+        return numbers;
+    }
+    
+    
+    
 }
